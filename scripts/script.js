903abc57e9fa3548eb6638638a81ae38a3cd77e2
Jmukumzoda <jmukumzoda63@gmail.com>
2023-09-29 14:53:25 +0500

    updte

--- scripts/script.js
+++ scripts/script.js
@@ -78,6 +78,7 @@ function reload(arr) {
         title_text.ondblclick = () => {
             let pr = prompt("text")
             title_text.textContent = pr
+            title_text.classList.toggle('text_dic')
         }
 
     }
